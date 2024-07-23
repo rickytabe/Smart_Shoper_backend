a259be776db22e77ac03c56e164489a941bdf7a7
rickytabe <rickytabe2@gmail.com>
2024-07-23 20:55:59 +0100

    Update to use certificate from environment variable

--- config/db.js
+++ config/db.js
@@ -1,9 +1,22 @@
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const dotenv = require('dotenv');
+const path = require('path');
 
 // Load environment variables
 dotenv.config();
+
+// Path to the certificate file, which will be set by Render
+const certPath = process.env.CERT_PEM_PATH || path.join(__dirname, 'cert.pem');
+
+// Read the certificate content
+let certContent;
+try {
+  certContent = fs.readFileSync(certPath);
+} catch (error) {
+  console.error(`Error reading certificate from ${certPath}:`, error);
+  process.exit(1);
+}
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
@@ -11,7 +24,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
   dialect: 'mysql',
   dialectOptions: {
     ssl: {
-      ca: fs.readFileSync(__dirname + '/cert.pem')
+      ca: certContent
     }
   }
 });
