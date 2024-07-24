90be32d1295820143de831793fd049d3f3ae3310
rickytabe <rickytabe2@gmail.com>
2024-07-24 13:05:02 +0100

    added auto model changes sync to database

--- models/Recommendation.js
+++ models/Recommendation.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+const sequelize = require('../config/db').sequelize;
 const User = require('./User');
 const Product = require('./Product');
 
--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
 const { DataTypes } = require('sequelize');
-const sequelize = require('../config/db');
+const sequelize = require('../config/db').sequelize;
 
 const User = sequelize.define('User', {
   username: {
